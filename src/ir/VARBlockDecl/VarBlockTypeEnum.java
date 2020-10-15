dba2823073c68077f014a8b264746598a92d0853
geo <geoffreyhe2@gmail.com>
2020-10-15 17:13:38 +0800

    TypeEnum， 各种类型的type也许不用都用class表示，直接用枚举，等待后面尝试

---
+++ src/ir/VARBlockDecl/VarBlockTypeEnum.java
@@ -0,0 +1,10 @@
+package ir.VARBlockDecl;
+
+public enum VarBlockTypeEnum {
+    /**可能用得上*/
+    VAR, VAR_OUTPUT,  VAR_INPUT, VAR_INPUT_OUTPUT, RES_BOOL, RES_SINT, RES_INT, RES_DINT,
+    RES_LINT, REAL, LREAL, USINT, UINT, UDINT, ULINT,
+    DATE, TIME_OF_DAY, TOD, DATE_AND_TIME, DT, BYTE, WORD,
+    DWORD, LWORD
+
+}
